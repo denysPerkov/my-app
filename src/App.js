b14af41d1c9fd03232b1766e530824f2f0e65474
Denys Perkov <deztrouble@gmail.com>
2025-07-10 17:02:14 +0200

    CRUD operations for Todo items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import './App.css';
-import TodoTable from './components/TodoTable';
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+import React, { useState } from "react";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
 
-  const todos = [
-    {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
-    {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
-    {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User One'},
-    {rowNumber: 4, rowDescription: 'Charge phone', rowAssigned: 'User One'},
-  ]
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User One" },
+    { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User Two" },
+    { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User One" },
+    { rowNumber: 4, rowDescription: "Charge phone", rowAssigned: "User One" },
+  ]);
+
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
+
+    const newTodo = {
+      rowNumber: todos.length + 1,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+    setTodos((todos) => [...todos, newTodo]);
+  };
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  };
 
   return (
-    <div className='mt-5 spaceiner'>
-      <div className='card'>
-        <div className='card-header'>
-          Your Todo's
-        </div>
-        <div className='card-body'>
-          <TodoTable todos={todos}/>
+    <div className="mt-5 spaceiner">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
+          <button
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+            className="btn btn-primary"
+          >
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
+          </button>
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
         </div>
       </div>
     </div>
